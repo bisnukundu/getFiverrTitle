97dae3dbb015fe77f332800d3d5ce42a0963d2ab
bisnukundu <bisnukundu@gmail.com>
2021-01-12 23:21:57 +0600

    Now Download gig_titles with file

--- code.js
+++ code.js
@@ -6,13 +6,25 @@ var count_title = title.length
 var count_value = 0;
 console.log(`Total Title = ${count_title}`)
 console.log(`Total Rating = ${count_rating}`)
+// Download function 
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+    element.style.display = 'none';
+    document.body.appendChild(element);
+    element.click();
+    document.body.removeChild(element);
+}
+var all_titles = [];
 
 for (let i = 0; i < count_rating; i++) {
     if (rating[i].innerText == "(1k+)") {
         console.log(`${title[i].innerText}   (Rating = ${rating[i].innerText})`)
+        all_titles.push("\n" + title[i].innerText)
         count_value++
     }
 
 }
-
+download("Gig_Title.txt", all_titles);
 console.log(`Totle Found = ${count_value}`)
