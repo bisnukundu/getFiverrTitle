cdfb10c69c0198b13cc8fef0de61e83f6cb15850
bisnukundu <bisnukundu@gmail.com>
2021-01-12 23:24:53 +0600

    Now will be Download Gig Title in Gig_Title.txt

--- code.js
+++ code.js
@@ -6,7 +6,7 @@ var count_title = title.length
 var count_value = 0;
 console.log(`Total Title = ${count_title}`)
 console.log(`Total Rating = ${count_rating}`)
-// Download function 
+//This is  Download function 
 function download(filename, text) {
     var element = document.createElement('a');
     element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
